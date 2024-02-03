ab94c811408a8f9b950751827b3cdad0c0940ba4
heligie <heligiette@gmail.com>
2024-02-04 02:48:39 +0300

    feat: change loader

--- src/view/stubs/loader-view.js
+++ src/view/stubs/loader-view.js
@@ -1,6 +1,14 @@
 import AbstractView from '../../framework/view/abstract-view.js';
 
-const createLoaderTemplate = () => '<p class="trip-events__msg">Loading...</p>';
+const createLoaderTemplate = () => (/*html*/
+  `<div class='loader-container'>
+    <div class="loader">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </div>`
+);
 
 export default class LoaderView extends AbstractView {
   get template() {
